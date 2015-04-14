bdab607ada4d7a5f30b6958dea7e4a34a1b7bc9c
Poonam Joshi <Poonam@Poonams-MacBook.local>
2015-04-14 13:38:53 +0100

    added a new class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Poonam Joshi on 14/04/2015.
+//  Copyright (c) 2015 My Projects. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Poonam Joshi on 14/04/2015.
+//  Copyright (c) 2015 My Projects. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
